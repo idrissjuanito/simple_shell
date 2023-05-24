12599a5a95b11a7a99ed0423d96740360ffdcc9f
Bright Thompson <brightthompson35@gmail.com>
2023-05-24 14:23:06 -0700

    error

---
+++ error.c
@@ -0,0 +1,16 @@
+#include <errno.h>
+#include "utils.h"
+/**
+ * exitOnError - exits the current process in case of error
+ *
+ * @shell: the running shell
+ * @cmd: command typed
+ *
+ * Return: nothing
+ */
+void exitOnError(char *shell, char *cmd)
+{
+	if (cmd)
+		fprintf(stderr, "%s: %s\n", shell, strerror(errno));
+	exit(EXIT_FAILURE);
+}
